759b56c84237eebe42fd7f5a0b8573c669a1f4c6
astartsevv <Startsev2001@list.ru>
2022-10-08 22:34:17 +0500

    Add z1.py

---
+++ z1.py
@@ -0,0 +1,13 @@
+#Список: семья
+family = ['Father','Mother','Brother','Sister']
+print (family)
+#Список: состав группы
+group = ['Aleksey','Danil','Egor','Ivan']
+print (group [1])
+#Список: зачетная книжка студента
+report_card = ['Metrology','TAU','Programming','PE']
+print (report_card [0])
+
+
+
+
